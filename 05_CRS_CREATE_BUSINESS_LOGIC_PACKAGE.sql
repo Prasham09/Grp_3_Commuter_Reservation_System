0eac54b460d80dd1c8eca93d4b6200769dae4434
Prasham09 <nagda.pr@northeastern.edu>
2025-12-02 17:40:57 -0600

    Create PKG

--- 05_CRS_CREATE_BUSINESS_LOGIC_PACKAGE.sql
+++ 05_CRS_CREATE_BUSINESS_LOGIC_PACKAGE.sql
@@ -2,3 +2,12 @@
 -- SCRIPT 5: CREATE_BUSINESS_LOGIC_PACKAGE.sql
 --️ MUST RUN AS CRS_ADMIN ️
 -- ============================================
+
+
+-- ============================================
+-- PACKAGE SPECIFICATION
+-- ============================================
+CREATE OR REPLACE PACKAGE CRS_BOOKING_PKG AS
+
+END CRS_BOOKING_PKG;
+/
